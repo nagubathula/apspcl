087e7344f366901e876ce3249ff809e8a28ec0aa
nagubathula <satyasai2108@gmail.com>
2024-06-28 11:37:35 +0530

    Changes in Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@ import Header from "@/components/Header1";
 import Navbar from "@/components/Navbar";
 import ImageSlider from "@/components/ImageSlider";
 import QuickLink from "@/components/QuickLink";
+import QuickLink2 from "@/components/QuickLink2";
 import Image from "next/image";
 import AboutApspcl from "@/components/AboutApspcl";
 import Carousel from "@/components/Carousel";
 import FooterLinks from "@/components/FooterLinks";
+import People from "@/components/People";
+import About from "@/components/About";
 // import footerLinksData from '@/data/footerLinks.json';
 // Dummy data for cards
 const cards = [
@@ -29,18 +32,30 @@ const cards = [
 
 export default function Home() {
   return (
-    <div>
-     
-      <div className="max-w-7xl mx-auto flex flex-col gap-2">
-        
-        <ImageSlider />
-        <QuickLink />
-        <AboutApspcl />
-        <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-4">Card Carousel</h1>
-          <Carousel cards={cards} />
+    <div className="relative">
+      <div className="relative z-50">
+        <Header />
+        <Navbar />
+      </div>
+      <div className="my-8 max-w-7xl mx-auto flex flex-row gap-8 py-8">
+        <div className="w-9/12">
+          <ImageSlider />
+          {/* <QuickLink2 /> */}
+          {/* <AboutApspcl /> */}
+          <div className="bg-gray-100">
+            <About />
+          </div>
+          <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Card Carousel</h1>
+            <Carousel cards={cards} />
+          </div>
+        </div>
+        <div id="sticky" className="w-3/12 flex flex-col gap-4">
+          <People />
+          <QuickLink />
         </div>
       </div>
+
       <FooterLinks />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import menuData from "@/data/menuData.json";
 import { MenuIcon, XIcon, ChevronDownIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activeMenuIndex, setActiveMenuIndex] = useState(null);
+  const timeoutRef = useRef(null);
 
   // Function to toggle active menu index for desktop view
   const toggleActiveMenu = (index) => {
     setActiveMenuIndex(activeMenuIndex === index ? null : index);
   };
 
+  const handleMouseEnter = (index) => {
+    clearTimeout(timeoutRef.current);
+    setActiveMenuIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setActiveMenuIndex(null);
+    }, 300); // Adjust delay as needed
+  };
+
   return (
-    <div className=" z-50 bg-blue-600 shadow-md">
-      <nav className="container mx-auto px-4 py-6 md:px-2">
+    <div className="z-50 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 shadow-md">
+      <nav className="container mx-auto px-4 py-3 ">
         <div className="flex justify-between items-center">
           {/* Menu Toggle for Mobile */}
           <div className="md:hidden">
@@ -36,12 +48,12 @@ const Navbar = () => {
               <div
                 key={index}
                 className="relative group"
-                onMouseEnter={() => toggleActiveMenu(index)}
-                onMouseLeave={() => setActiveMenuIndex(null)}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
               >
                 <a
                   href={menuItem.link}
-                  className="text-white hover:text-blue-400 uppercase tracking-wide flex items-center"
+                  className="text-white hover:text-blue-300 uppercase tracking-wide flex items-center transition-colors duration-300"
                 >
                   {menuItem.label}
                   {menuItem.subItems.length > 0 && (
@@ -51,19 +63,17 @@ const Navbar = () => {
                 {/* Submenu for Desktop */}
                 {menuItem.subItems.length > 0 && (
                   <ul
-                    className={`absolute left-0 hidden group-hover:block bg-blue-800 text-white rounded-md py-1 mt-2 ${
-                      activeMenuIndex === index
-                        ? "opacity-100"
-                        : "opacity-0 pointer-events-none"
+                    className={`absolute left-0 hidden group-hover:block bg-blue-600 text-white rounded-md py-1  shadow-lg ${
+                      activeMenuIndex === index ? "block" : "hidden"
                     } transition-opacity duration-300`}
-                    onMouseEnter={() => setActiveMenuIndex(index)}
-                    onMouseLeave={() => setActiveMenuIndex(null)}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={handleMouseLeave}
                   >
                     {menuItem.subItems.map((subItem, subIndex) => (
                       <li key={subIndex}>
                         <a
                           href={subItem.link}
-                          className="block px-4 py-2 hover:bg-blue-400"
+                          className="block px-4 py-2 hover:bg-blue-500 transition-colors duration-300"
                         >
                           {subItem.label}
                         </a>
@@ -79,12 +89,12 @@ const Navbar = () => {
         {/* Mobile Menu */}
         {menuOpen && (
           <div className="md:hidden mt-2">
-            <ul className="bg-gray-800 text-white rounded-md py-1">
+            <ul className="bg-blue-600 text-white rounded-md py-1 shadow-lg">
               {menuData.menuItems.map((menuItem, index) => (
                 <li key={index}>
                   <a
                     href={menuItem.link}
-                    className="px-4 py-2 hover:bg-blue-400 flex items-center justify-between"
+                    className="px-4 py-2 hover:bg-blue-500 flex items-center justify-between transition-colors duration-300"
                     onClick={() => setMenuOpen(false)} // Close menu on mobile link click
                   >
                     <span>{menuItem.label}</span>
@@ -94,12 +104,12 @@ const Navbar = () => {
                   </a>
                   {/* Submenu for Mobile */}
                   {menuItem.subItems.length > 0 && (
-                    <ul className="pl-4">
+                    <ul className="pl-4 bg-blue-700">
                       {menuItem.subItems.map((subItem, subIndex) => (
                         <li key={subIndex}>
                           <a
                             href={subItem.link}
-                            className="block px-4 py-2 hover:bg-blue-400"
+                            className="block px-4 py-2 hover:bg-blue-500 transition-colors duration-300"
                           >
                             {subItem.label}
                           </a>
