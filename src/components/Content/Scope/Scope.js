fe0918d676c1d26ecf761b8d2a6d5341997b62c8
nagubathula <satyasai2108@gmail.com>
2024-06-28 14:21:12 +0530

    Changes

--- src/components/Content/Scope/Scope.js
+++ src/components/Content/Scope/Scope.js
@@ -1,62 +1,40 @@
 "use client";
-
 import React, { useState } from "react";
-import {
-  MenuIcon,
-  ChevronDownIcon,
-  HomeIcon,
-  CogIcon,
-  UserIcon,
-} from "@heroicons/react/solid";
-
-const icons = {
-  MenuIcon: MenuIcon,
-  HomeIcon: HomeIcon,
-  CogIcon: CogIcon,
-  UserIcon: UserIcon,
-  ChevronDownIcon: ChevronDownIcon,
-};
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 const menuData = {
   menuItems: [
     {
       title: "Menu Item 1",
-      icon: "MenuIcon",
       link: "#",
       dropdown: true,
       dropdownItems: [
         {
           title: "Dropdown Item 1",
-          icon: "UserIcon",
           link: "#",
         },
         {
           title: "Dropdown Item 2",
-          icon: "UserIcon",
           link: "#",
         },
         {
           title: "Dropdown Item 3",
-          icon: "UserIcon",
           link: "#",
         },
       ],
     },
     {
       title: "Menu Item 2",
-      icon: "HomeIcon",
       link: "#",
       dropdown: false,
     },
     {
       title: "Menu Item 3",
-      icon: "CogIcon",
       link: "#",
       dropdown: false,
     },
     {
       title: "Menu Item 4",
-      icon: "UserIcon",
       link: "#",
       dropdown: false,
     },
@@ -71,63 +49,80 @@ const Scope = () => {
   };
 
   return (
-    <div className="flex min-h-screen">
-      <div className="w-1/4 bg-gray-800 text-white p-4">
+    <div className="flex min-h-[36rem]">
+      <div className="w-1/4 bg-gray-200 text-gray-800 p-4">
         <nav>
           <ul>
-            {menuData.menuItems.map((item, index) => {
-              const Icon = icons[item.icon];
-              return (
-                <li key={index} className="mb-4">
-                  {item.dropdown ? (
-                    <>
-                      <button
-                        className="w-full text-left flex items-center"
-                        onClick={() => toggleDropdown(index)}
-                      >
-                        <Icon className="h-5 w-5 mr-2" />
-                        {item.title}
-                        <ChevronDownIcon className="h-5 w-5 ml-auto" />
-                      </button>
-                      {openDropdown === index && (
-                        <ul className="ml-4 mt-2">
-                          {item.dropdownItems.map((dropdownItem, idx) => {
-                            const DropdownIcon = icons[dropdownItem.icon];
-                            return (
-                              <li key={idx} className="mb-2 flex items-center">
-                                <DropdownIcon className="h-5 w-5 mr-2" />
-                                <a href={dropdownItem.link}>
-                                  {dropdownItem.title}
-                                </a>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      )}
-                    </>
-                  ) : (
-                    <a
-                      href={item.link}
-                      className="w-full text-left flex items-center"
+            {menuData.menuItems.map((item, index) => (
+              <li key={index} className="mb-4">
+                {item.dropdown ? (
+                  <>
+                    <button
+                      className="w-full text-left flex items-center py-2 px-4 rounded-lg hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
+                      onClick={() => toggleDropdown(index)}
                     >
-                      <Icon className="h-5 w-5 mr-2" />
-                      {item.title}
-                    </a>
-                  )}
-                </li>
-              );
-            })}
+                      <span className="text-sm font-medium">{item.title}</span>
+                      <ChevronDownIcon className="h-5 w-5 ml-auto" />
+                    </button>
+                    {openDropdown === index && (
+                      <ul className="ml-4 mt-2">
+                        {item.dropdownItems.map((dropdownItem, idx) => (
+                          <li key={idx} className="mb-2">
+                            <a
+                              href={dropdownItem.link}
+                              className="block py-2 px-4 rounded-lg hover:bg-gray-300"
+                            >
+                              {dropdownItem.title}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </>
+                ) : (
+                  <a
+                    href={item.link}
+                    className="w-full text-left py-2 px-4 rounded-lg block hover:bg-gray-300"
+                  >
+                    <span className="text-sm font-medium">{item.title}</span>
+                  </a>
+                )}
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
-      <div className="w-3/4 bg-white p-4">
+      <div className="w-3/4 bg-white p-4 ">
         <h1 className="text-2xl font-bold mb-4">Scope</h1>
-        <ul className="list-disc list-inside">
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-        </ul>
+        <div className="h-full overflow-y-auto">
+          <ul className="list-disc list-outside pl-6">
+            <li className="mb-4">
+              <span className="block">
+                The Scope of JVC is Conceptualization, Structuring,
+                Implementation, Operation and Maintenance of Solar Parks at the
+                sites in the state of Andhra Pradesh by optimum utilization of
+                resources and knowledge base of Parties.
+              </span>
+            </li>
+            <li className="mb-4">
+              <span className="block">
+                The JVC shall (i) plan, develop and operate solar park through a
+                suitable revenue model, and (ii) plan such other Solar Projects
+                as a developer or under any other arrangement as may be decided
+                by the JVC from time to time.
+              </span>
+            </li>
+            <li className="mb-4">
+              <span className="block">
+                In addition to develop solar parks, JVC will also be setting up
+                ultra-mega and other solar power projects with financial support
+                from Goverment of lndia (budgetary support as well as support
+                from NCEF), State Government and various multi/bi-lateral
+                financial institutions.
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
